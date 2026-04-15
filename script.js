35dac316bbb0b8f4bb9ba21e35ba224ea0d07704
marawalter <marawalter98@gmail.com>
2026-04-15 10:43:21 +0200

    Enhance index.html, script.js, and styles.css with collapsible sections and improved styling

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function helloWorld() {
     alert("Hello World!");
 }
- function Willkommen() {
+function Willkommen() {
     alert("Schön, dass du da bist :)")
- }
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
